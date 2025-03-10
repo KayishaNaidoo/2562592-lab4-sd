e3be022b8dd48d69d91286de7c2af4a2223be25a
Kayisha naidoo <naidookayisha8@gmail.com>
2025-03-10 16:59:43 +0200

    refactor country information display and enhance neighbor handling with images

--- script.js
+++ script.js
@@ -5,65 +5,51 @@ btn.addEventListener("click", function () {
   getCountryArr(country);
 });
 
-updateContent = (countryArray) => {
+async function updateContent(countryArray) {
   const countryInfoC = document.getElementById("country-info");
   countryInfoC.innerHTML = "";
+
   if (countryArray.error) {
     countryInfoC.innerHTML = `<p>${countryArray.error}</p>`;
     return;
   }
-  //checking if neighbors is empty
-  neighbourHTML = "";
-  if (countryArray.neighbours == undefined) {
-    countryArray.neighbours = "None";
-  } else {
-    countryArray.neighbours = countryArray.neighbours.join(", ");
+
+  let neighbourHTML = "<p>None</p>";
+  if (countryArray.neighbours && countryArray.neighbours.length > 0) {
+    const neighborsImages = await getNeighbourImages(countryArray.neighbours);
+    neighbourHTML = "";
     for (let i = 0; i < countryArray.neighbours.length; i++) {
-      neighbourHTML += `<p>${countryArray.neighbours[i]}</p>`;
+      neighbourHTML += ` <img src="${neighborsImages[i]}" alt="${countryArray.neighbours[i]}'s Flag" width="50"/>
+                          <p>${countryArray.neighbours[i]}</p>
+                       `;
     }
   }
+
   const CountryInfoHTML = `
         <img src="${countryArray.flag}" alt="${countryArray.name}'s Flag" />
         <h2>${countryArray.name}</h2>
         <p><strong>Capital:</strong> ${countryArray.capital}</p>
         <p><strong>Population:</strong> ${countryArray.population}</p>
         <p><strong>Region:</strong> ${countryArray.region}</p>
-        <p><strong>Neighbours:</strong> ${countryArray.neighbours}</p>
-        ${neighbourHTML}
+        <p><strong>Neighbours:</strong></p>
+       ${neighbourHTML}
     `;
 
   countryInfoC.innerHTML = CountryInfoHTML;
-};
-
-async function getCountryImage(country_name) {
-  await fetch(`https://restcountries.com/v3.1/name/${country_name}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Country not found or misspelled");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const countryInfo = data[0];
-      const countryFlag = countryInfo.flags.png;
-      console.log(countryInfo.name);
-      return countryFlag;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 }
 
 async function getCountryArr() {
   const countryN = country_name.value;
+
   await fetch(`https://restcountries.com/v3.1/name/${countryN}`)
     .then((response) => {
       if (!response.ok) {
+        alert("Country not found or misspelled:(");
         throw new Error("Country not found or misspelled");
       }
       return response.json();
     })
-    .then((data) => {
+    .then(async (data) => {
       const countryStuff = data[0];
       const countryArray = {
         name: countryStuff.name.common,
@@ -71,13 +57,59 @@ async function getCountryArr() {
         population: countryStuff.population,
         region: countryStuff.region,
         flag: countryStuff.flags.png,
-        neighbours: countryStuff.borders,
-        colors: countryStuff.colors,
+        neighbours: await getNeighbourNames(countryStuff.borders),
       };
-      console.log(countryStuff.name);
-      updateContent(countryArray);
+      await updateContent(countryArray);
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+async function getNeighbourNames(neighbours) {
+  if (!neighbours) return [];
+  const neighbourNames = [];
+
+  for (let i = 0; i < neighbours.length; i++) {
+    await fetch(`https://restcountries.com/v3.1/alpha/${neighbours[i]}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Country not found or misspelled");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const neighbour = data[0];
+        neighbourNames.push(neighbour.name.common);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return neighbourNames;
+}
+
+async function getNeighbourImages(neighbours) {
+  if (!neighbours) return [];
+  const neighbourFlags = [];
+
+  for (let i = 0; i < neighbours.length; i++) {
+    await fetch(`https://restcountries.com/v3.1/name/${neighbours[i]}`)
+      .then((response) => {
+        if (!response.ok) {
+          alert("Country not found or misspelled");
+          throw new Error("Country not found or misspelled");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const neighbour = data[0];
+        neighbourFlags.push(neighbour.flags.png);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return neighbourFlags;
 }
